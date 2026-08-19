fccdae6395c490e13f4947680e112b5a9844f34e
GabrielBeckler <gabrielbhte0@gmail.com>
2026-08-19 11:27:22 -0300

    Adicionado funções, salvar, alterar, deletar e carregar

--- PROJETO17/MainForm.Designer.cs
+++ PROJETO17/MainForm.Designer.cs
@@ -29,7 +29,7 @@ namespace PROJETO17
 		private System.Windows.Forms.TextBox txtpesquisar;
 		private System.Windows.Forms.PictureBox pictureBox1;
 		private System.Windows.Forms.Label label4;
-		private System.Windows.Forms.MaskedTextBox mskfone;
+		private System.Windows.Forms.MaskedTextBox txtfone;
 		
 		/// <summary>
 		/// Disposes resources used by the form.
@@ -44,11 +44,10 @@ namespace PROJETO17
 			}
 			base.Dispose(disposing);
 		}
-		
+
 		/// <summary>
-		/// This method is required for Windows Forms designer support.
-		/// Do not change the method contents inside the source code editor. The Forms designer might
-		/// not be able to load this method if it was changed manually.
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
 		/// </summary>
 		private void InitializeComponent()
 		{
@@ -68,7 +67,7 @@ namespace PROJETO17
 			this.txtpesquisar = new System.Windows.Forms.TextBox();
 			this.pictureBox1 = new System.Windows.Forms.PictureBox();
 			this.label4 = new System.Windows.Forms.Label();
-			this.mskfone = new System.Windows.Forms.MaskedTextBox();
+			this.txtfone = new System.Windows.Forms.MaskedTextBox();
 			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
 			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
 			this.SuspendLayout();
@@ -76,115 +75,122 @@ namespace PROJETO17
 			// label1
 			// 
 			this.label1.Cursor = System.Windows.Forms.Cursors.Default;
-			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label1.Location = new System.Drawing.Point(23, 26);
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(29, 67);
 			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(100, 23);
+			this.label1.Size = new System.Drawing.Size(110, 35);
 			this.label1.TabIndex = 0;
-			this.label1.Text = "Nome";
+			this.label1.Text = "Nome:\r\n";
 			// 
 			// txtnome
 			// 
 			this.txtnome.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
-			this.txtnome.Location = new System.Drawing.Point(23, 52);
+			this.txtnome.Location = new System.Drawing.Point(38, 138);
 			this.txtnome.MaxLength = 50;
 			this.txtnome.Name = "txtnome";
-			this.txtnome.Size = new System.Drawing.Size(379, 20);
+			this.txtnome.Size = new System.Drawing.Size(389, 23);
 			this.txtnome.TabIndex = 2;
+			this.txtnome.TextChanged += new System.EventHandler(this.txtnome_TextChanged);
 			// 
 			// txtemail
 			// 
 			this.txtemail.CharacterCasing = System.Windows.Forms.CharacterCasing.Lower;
-			this.txtemail.Location = new System.Drawing.Point(22, 131);
+			this.txtemail.Location = new System.Drawing.Point(38, 290);
 			this.txtemail.MaxLength = 150;
 			this.txtemail.Name = "txtemail";
-			this.txtemail.Size = new System.Drawing.Size(379, 20);
+			this.txtemail.Size = new System.Drawing.Size(389, 23);
 			this.txtemail.TabIndex = 4;
 			// 
 			// label2
 			// 
 			this.label2.Cursor = System.Windows.Forms.Cursors.Default;
-			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label2.Location = new System.Drawing.Point(22, 113);
+			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.Location = new System.Drawing.Point(29, 211);
 			this.label2.Name = "label2";
-			this.label2.Size = new System.Drawing.Size(100, 23);
+			this.label2.Size = new System.Drawing.Size(110, 47);
 			this.label2.TabIndex = 3;
-			this.label2.Text = "Email";
+			this.label2.Text = "Email:\r\n\r\n";
 			// 
 			// btnadd
 			// 
 			this.btnadd.BackColor = System.Drawing.SystemColors.AppWorkspace;
-			this.btnadd.Location = new System.Drawing.Point(22, 169);
+			this.btnadd.Location = new System.Drawing.Point(38, 347);
 			this.btnadd.Name = "btnadd";
-			this.btnadd.Size = new System.Drawing.Size(114, 38);
+			this.btnadd.Size = new System.Drawing.Size(217, 73);
 			this.btnadd.TabIndex = 5;
 			this.btnadd.Text = "Adicionar";
 			this.btnadd.UseVisualStyleBackColor = false;
+			this.btnadd.Click += new System.EventHandler(this.btnadd_Click);
 			// 
 			// btnsalvar
 			// 
 			this.btnsalvar.BackColor = System.Drawing.SystemColors.AppWorkspace;
-			this.btnsalvar.Location = new System.Drawing.Point(196, 169);
+			this.btnsalvar.Location = new System.Drawing.Point(279, 347);
 			this.btnsalvar.Name = "btnsalvar";
-			this.btnsalvar.Size = new System.Drawing.Size(115, 38);
+			this.btnsalvar.Size = new System.Drawing.Size(255, 66);
 			this.btnsalvar.TabIndex = 6;
 			this.btnsalvar.Text = "Salvar";
 			this.btnsalvar.UseVisualStyleBackColor = false;
+			this.btnsalvar.Click += new System.EventHandler(this.btnsalvar_Click);
 			// 
 			// btnalterar
 			// 
 			this.btnalterar.BackColor = System.Drawing.SystemColors.AppWorkspace;
-			this.btnalterar.Location = new System.Drawing.Point(367, 169);
+			this.btnalterar.Location = new System.Drawing.Point(580, 347);
 			this.btnalterar.Name = "btnalterar";
-			this.btnalterar.Size = new System.Drawing.Size(128, 38);
+			this.btnalterar.Size = new System.Drawing.Size(264, 66);
 			this.btnalterar.TabIndex = 7;
 			this.btnalterar.Text = "Alterar";
 			this.btnalterar.UseVisualStyleBackColor = false;
+			this.btnalterar.Click += new System.EventHandler(this.btnalterar_Click);
 			// 
 			// btnexcluir
 			// 
 			this.btnexcluir.BackColor = System.Drawing.SystemColors.AppWorkspace;
-			this.btnexcluir.Location = new System.Drawing.Point(532, 169);
+			this.btnexcluir.Location = new System.Drawing.Point(904, 347);
 			this.btnexcluir.Name = "btnexcluir";
-			this.btnexcluir.Size = new System.Drawing.Size(122, 38);
+			this.btnexcluir.Size = new System.Drawing.Size(278, 66);
 			this.btnexcluir.TabIndex = 8;
 			this.btnexcluir.Text = "Excluir";
 			this.btnexcluir.UseVisualStyleBackColor = false;
+			this.btnexcluir.Click += new System.EventHandler(this.btnexcluir_Click);
 			// 
 			// dataGridView1
 			// 
 			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-			this.dataGridView1.Location = new System.Drawing.Point(23, 213);
+			this.dataGridView1.Location = new System.Drawing.Point(38, 439);
 			this.dataGridView1.Name = "dataGridView1";
-			this.dataGridView1.Size = new System.Drawing.Size(472, 129);
+			this.dataGridView1.Size = new System.Drawing.Size(1144, 294);
 			this.dataGridView1.TabIndex = 9;
+			this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
 			// 
 			// label3
 			// 
 			this.label3.Cursor = System.Windows.Forms.Cursors.Default;
-			this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label3.Location = new System.Drawing.Point(28, 367);
+			this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label3.Location = new System.Drawing.Point(70, 772);
 			this.label3.Name = "label3";
-			this.label3.Size = new System.Drawing.Size(156, 23);
+			this.label3.Size = new System.Drawing.Size(316, 44);
 			this.label3.TabIndex = 10;
 			this.label3.Text = "Digite nome para consulta:";
 			// 
 			// btnconsultartodos
 			// 
 			this.btnconsultartodos.BackColor = System.Drawing.SystemColors.AppWorkspace;
-			this.btnconsultartodos.Location = new System.Drawing.Point(515, 311);
+			this.btnconsultartodos.Location = new System.Drawing.Point(1221, 571);
 			this.btnconsultartodos.Name = "btnconsultartodos";
-			this.btnconsultartodos.Size = new System.Drawing.Size(139, 34);
+			this.btnconsultartodos.Size = new System.Drawing.Size(452, 120);
 			this.btnconsultartodos.TabIndex = 11;
 			this.btnconsultartodos.Text = "Consulta Todos";
 			this.btnconsultartodos.UseVisualStyleBackColor = false;
+			this.btnconsultartodos.Click += new System.EventHandler(this.btnconsultartodos_Click);
 			// 
 			// btnpesquisar
 			// 
 			this.btnpesquisar.BackColor = System.Drawing.SystemColors.AppWorkspace;
-			this.btnpesquisar.Location = new System.Drawing.Point(515, 350);
+			this.btnpesquisar.Location = new System.Drawing.Point(1221, 697);
 			this.btnpesquisar.Name = "btnpesquisar";
-			this.btnpesquisar.Size = new System.Drawing.Size(139, 34);
+			this.btnpesquisar.Size = new System.Drawing.Size(452, 119);
 			this.btnpesquisar.TabIndex = 12;
 			this.btnpesquisar.Text = "Pesquisar";
 			this.btnpesquisar.UseVisualStyleBackColor = false;
@@ -192,48 +198,47 @@ namespace PROJETO17
 			// txtpesquisar
 			// 
 			this.txtpesquisar.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
-			this.txtpesquisar.Location = new System.Drawing.Point(168, 364);
+			this.txtpesquisar.Location = new System.Drawing.Point(392, 778);
 			this.txtpesquisar.MaxLength = 50;
 			this.txtpesquisar.Name = "txtpesquisar";
-			this.txtpesquisar.Size = new System.Drawing.Size(327, 20);
+			this.txtpesquisar.Size = new System.Drawing.Size(790, 23);
 			this.txtpesquisar.TabIndex = 13;
 			// 
 			// pictureBox1
 			// 
 			this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
 			this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-			this.pictureBox1.Location = new System.Drawing.Point(487, 5);
+			this.pictureBox1.Location = new System.Drawing.Point(1201, 38);
 			this.pictureBox1.Name = "pictureBox1";
-			this.pictureBox1.Size = new System.Drawing.Size(167, 92);
+			this.pictureBox1.Size = new System.Drawing.Size(472, 275);
 			this.pictureBox1.TabIndex = 14;
 			this.pictureBox1.TabStop = false;
-			this.pictureBox1.Click += new System.EventHandler(this.PictureBox1Click);
 			// 
 			// label4
 			// 
 			this.label4.Cursor = System.Windows.Forms.Cursors.Default;
-			this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label4.Location = new System.Drawing.Point(487, 116);
+			this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label4.Location = new System.Drawing.Point(1201, 358);
 			this.label4.Name = "label4";
-			this.label4.Size = new System.Drawing.Size(150, 23);
+			this.label4.Size = new System.Drawing.Size(160, 25);
 			this.label4.TabIndex = 15;
 			this.label4.Text = "Telefone";
-			this.label4.Click += new System.EventHandler(this.Label4Click);
 			// 
-			// mskfone
+			// txtfone
 			// 
-			this.mskfone.Location = new System.Drawing.Point(487, 131);
-			this.mskfone.Mask = "(99)9999-9999";
-			this.mskfone.Name = "mskfone";
-			this.mskfone.Size = new System.Drawing.Size(167, 20);
-			this.mskfone.TabIndex = 17;
+			this.txtfone.Location = new System.Drawing.Point(1384, 360);
+			this.txtfone.Mask = "(99)9999-9999";
+			this.txtfone.Name = "txtfone";
+			this.txtfone.Size = new System.Drawing.Size(289, 23);
+			this.txtfone.TabIndex = 17;
 			// 
 			// frmCadastro
 			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(666, 403);
-			this.Controls.Add(this.mskfone);
+			this.BackColor = System.Drawing.SystemColors.InactiveCaption;
+			this.ClientSize = new System.Drawing.Size(1713, 1033);
+			this.Controls.Add(this.txtfone);
 			this.Controls.Add(this.label4);
 			this.Controls.Add(this.pictureBox1);
 			this.Controls.Add(this.txtpesquisar);
@@ -258,7 +263,6 @@ namespace PROJETO17
 			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
 			this.ResumeLayout(false);
 			this.PerformLayout();
-
 		}
 	}
 }
--- PROJETO17/MainForm.cs
+++ PROJETO17/MainForm.cs
@@ -1,63 +1,433 @@
-﻿/*
- * Created by SharpDevelop.
- * User: Usuário
- * Date: 17/06/2026
- * Time: 11:16
- * 
- * To change this template use Tools | Options | Coding | Edit Standard Headers.
- */
-using System;
-using System.Collections.Generic;
-using System.Drawing;
-using System.Windows.Forms;
-using MySql.Data;
-using MySql.Data.MySqlClient;
+﻿using System;
 using System.Data;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace PROJETO17
 {
-	/// <summary>
-	/// Description of MainForm.
-	/// </summary>
-	public partial class frmCadastro : Form
-	{
-		public frmCadastro()
-		{
-			InitializeComponent();
-			txtnome.Enabled=false;
-			txtemail.Enabled=false;
-			txtfone.Enablend=false;
-		}
-		
-		void Limpar()
-		{
-			sqlcon.Close();
-			txtname.Clear();
-			txtemail.Clear();
-			txtfone.Clear;
-			txtpesquisar.Clear();
-		}
-		
-		void CarregarDados()
-		{
-			String strsql = "SELECT codigo, nome, email, telefone FROM tbclient";
-			MySqlConnection conexao = new MySqlConnection(strcon);
-			try{
-				conexao.Open();
-				MySqlDataAdapter adaptador = new MySqlDataAdapter(strsql, conexao);
-				DataTable tabela = new DataTable();
-				adaptador.Fill(tabela);
-				dataGridView1.DataSource = tabela;
-			}
-			catch (Exception ex)
-			{
-				MessageBpox.Show("Erro ao carregar os clientes " + ex.Message);
-			}
-			finally
-			{
-				conexao.Close();
-			}
-		}
-		
-	}
+    public partial class frmCadastro : Form
+    {
+        private MySqlConnection sqlcon = null;
+
+        private string strcon =
+            "server=localhost;uid=root;password=123456;database=bdclientes2026";
+
+        private int idSelecionado = -1;
+
+        public frmCadastro()
+        {
+            InitializeComponent();
+
+            txtnome.Enabled = false;
+            txtemail.Enabled = false;
+            txtfone.Enabled = false;
+            txtpesquisar.Enabled = false;
+
+            // Carrega os clientes ao abrir o formulário
+            btnconsultartodos_Click(null, null);
+        }
+
+        // BOTÃO ADICIONAR
+ 
+        private void btnadd_Click(object sender, EventArgs e)
+        {
+            LimparCampos();
+
+            idSelecionado = -1;
+
+            txtnome.Enabled = true;
+            txtemail.Enabled = true;
+            txtfone.Enabled = true;
+            txtpesquisar.Enabled = true;
+
+            txtnome.Focus();
+        }
+
+        // TEXTBOX NOME
+        private void txtnome_TextChanged(object sender, EventArgs e)
+        {
+            // Não precisa fazer nada aqui.
+            // O evento pode permanecer vazio.
+        }
+        
+        // BOTÃO EXCLUIR / LIMPAR
+        private void btnexcluir_Click(object sender, EventArgs e)
+        {
+            if (idSelecionado == -1)
+            {
+                LimparCampos();
+
+                MessageBox.Show(
+                    "Nenhum cliente selecionado.",
+                    "Atenção",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Warning
+                );
+
+                return;
+            }
+
+            DialogResult resultado = MessageBox.Show(
+                "Deseja realmente excluir este cliente?",
+                "Confirmação",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question
+            );
+
+            if (resultado != DialogResult.Yes)
+                return;
+
+            string strsql =
+                "DELETE FROM tbcliente WHERE codigo = @codigo";
+
+            using (MySqlConnection conexao = new MySqlConnection(strcon))
+            {
+                try
+                {
+                    conexao.Open();
+
+                    using (MySqlCommand comando =
+                           new MySqlCommand(strsql, conexao))
+                    {
+                        comando.Parameters.AddWithValue(
+                            "@codigo",
+                            idSelecionado
+                        );
+
+                        comando.ExecuteNonQuery();
+                    }
+
+                    MessageBox.Show(
+                        "Cliente excluído com sucesso!",
+                        "Confirmação",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Information
+                    );
+
+                    LimparCampos();
+
+                    idSelecionado = -1;
+
+                    DesabilitarCampos();
+
+                    btnconsultartodos_Click(null, null);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(
+                        "Erro ao excluir cliente: " + ex.Message,
+                        "Erro",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Error
+                    );
+                }
+            }
+        }
+
+        
+        // BOTÃO SALVAR
+        
+        private void btnsalvar_Click(object sender, EventArgs e)
+        {
+            // Validação
+            if (string.IsNullOrWhiteSpace(txtnome.Text))
+            {
+                MessageBox.Show(
+                    "Digite o nome do cliente.",
+                    "Atenção",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Warning
+                );
+
+                txtnome.Focus();
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(txtemail.Text))
+            {
+                MessageBox.Show(
+                    "Digite o e-mail do cliente.",
+                    "Atenção",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Warning
+                );
+
+                txtemail.Focus();
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(txtfone.Text))
+            {
+                MessageBox.Show(
+                    "Digite o telefone do cliente.",
+                    "Atenção",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Warning
+                );
+
+                txtfone.Focus();
+                return;
+            }
+
+            string strsql =
+                "INSERT INTO tbcliente (nome, email, telefone) " +
+                "VALUES (@nome, @email, @telefone)";
+
+            using (MySqlConnection conexao =
+                   new MySqlConnection(strcon))
+            {
+                try
+                {
+                    conexao.Open();
+
+                    using (MySqlCommand comando =
+                           new MySqlCommand(strsql, conexao))
+                    {
+                        comando.Parameters.AddWithValue(
+                            "@nome",
+                            txtnome.Text
+                        );
+
+                        comando.Parameters.AddWithValue(
+                            "@email",
+                            txtemail.Text
+                        );
+
+                        comando.Parameters.AddWithValue(
+                            "@telefone",
+                            txtfone.Text
+                        );
+
+                        comando.ExecuteNonQuery();
+                    }
+
+                    MessageBox.Show(
+                        "Cliente cadastrado com sucesso!",
+                        "Confirmação",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Information
+                    );
+
+                    LimparCampos();
+
+                    idSelecionado = -1;
+
+                    DesabilitarCampos();
+
+                    btnconsultartodos_Click(null, null);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(
+                        "Erro ao cadastrar cliente: " + ex.Message,
+                        "Erro",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Error
+                    );
+                }
+            }
+        }
+
+        
+        // BOTÃO ALTERAR
+        private void btnalterar_Click(object sender, EventArgs e)
+        {
+            if (idSelecionado == -1)
+            {
+                MessageBox.Show(
+                    "Selecione um cliente para alterar.",
+                    "Atenção",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Warning
+                );
+
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(txtnome.Text))
+            {
+                MessageBox.Show(
+                    "Digite o nome do cliente.",
+                    "Atenção",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Warning
+                );
+
+                txtnome.Focus();
+                return;
+            }
+
+            string strsql =
+                "UPDATE tbcliente SET " +
+                "nome = @nome, " +
+                "email = @email, " +
+                "telefone = @telefone " +
+                "WHERE codigo = @codigo";
+
+            using (MySqlConnection conexao =
+                   new MySqlConnection(strcon))
+            {
+                try
+                {
+                    conexao.Open();
+
+                    using (MySqlCommand comando =
+                           new MySqlCommand(strsql, conexao))
+                    {
+                        comando.Parameters.AddWithValue(
+                            "@nome",
+                            txtnome.Text
+                        );
+
+                        comando.Parameters.AddWithValue(
+                            "@email",
+                            txtemail.Text
+                        );
+
+                        comando.Parameters.AddWithValue(
+                            "@telefone",
+                            txtfone.Text
+                        );
+
+                        comando.Parameters.AddWithValue(
+                            "@codigo",
+                            idSelecionado
+                        );
+
+                        comando.ExecuteNonQuery();
+                    }
+
+                    MessageBox.Show(
+                        "Cliente alterado com sucesso!",
+                        "Confirmação",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Information
+                    );
+
+                    LimparCampos();
+
+                    idSelecionado = -1;
+
+                    DesabilitarCampos();
+
+                    btnconsultartodos_Click(null, null);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(
+                        "Erro ao alterar cliente: " + ex.Message,
+                        "Erro",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Error
+                    );
+                }
+            }
+        }
+
+        
+        // SELECIONAR CLIENTE NO DATAGRIDVIEW
+        
+        private void dataGridView1_CellContentClick(
+            object sender,
+            DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex < 0)
+                return;
+
+            try
+            {
+                DataGridViewRow linha =
+                    dataGridView1.Rows[e.RowIndex];
+
+                idSelecionado =
+                    Convert.ToInt32(linha.Cells["codigo"].Value);
+
+                txtnome.Text =
+                    linha.Cells["nome"].Value.ToString();
+
+                txtemail.Text =
+                    linha.Cells["email"].Value.ToString();
+
+                txtfone.Text =
+                    linha.Cells["telefone"].Value.ToString();
+
+                txtnome.Enabled = true;
+                txtemail.Enabled = true;
+                txtfone.Enabled = true;
+                txtpesquisar.Enabled = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(
+                    "Erro ao selecionar cliente: " + ex.Message,
+                    "Erro",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error
+                );
+            }
+        }
+
+        // CONSULTAR TODOS
+        
+        private void btnconsultartodos_Click(
+            object sender,
+            EventArgs e)
+        {
+            string strsql =
+                "SELECT codigo, nome, email, telefone " +
+                "FROM tbcliente " +
+                "ORDER BY codigo";
+
+            using (MySqlConnection conexao =
+                   new MySqlConnection(strcon))
+            {
+                try
+                {
+                    conexao.Open();
+
+                    using (MySqlDataAdapter adaptador =
+                           new MySqlDataAdapter(strsql, conexao))
+                    {
+                        DataTable tabela = new DataTable();
+
+                        adaptador.Fill(tabela);
+
+                        dataGridView1.DataSource = tabela;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(
+                        "Erro ao carregar os clientes: " +
+                        ex.Message,
+                        "Erro",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Error
+                    );
+                }
+            }
+        }
+
+        
+        // LIMPAR CAMPOS
+        
+        private void LimparCampos()
+        {
+            txtnome.Clear();
+            txtemail.Clear();
+            txtfone.Clear();
+            txtpesquisar.Clear();
+        }
+
+        
+        // DESABILITAR CAMPOS
+        
+        private void DesabilitarCampos()
+        {
+            txtnome.Enabled = false;
+            txtemail.Enabled = false;
+            txtfone.Enabled = false;
+            txtpesquisar.Enabled = false;
+        }
+    }
 }
